c6c1f2514742e848a7131150b457c700e73c6557
tibigame <ryosukehashimoto@tmail.com>
2021-09-25 22:06:18 +0900

    初期設定

---
+++ Assets/Scripts/UI/Tittle/gameEnd.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace title
+{
+    public class gameEnd : MonoBehaviour
+    {
+        public void OnClick()//ゲームを終了する
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;//Unity上の場合、再生を停止する
+#else
+		Application.Quit();//そのほかの場合、ゲームを終了する
+#endif
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Tittle/selectMode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace title
+{
+    public class selectMode : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject selectModePanel;
+        private bool isSlectMode;
+        void Start()
+        {
+            selectModePanel.SetActive(false);
+            isSlectMode = false;
+        }
+
+        public void Onclick()
+        {
+            if (isSlectMode == false)
+            {
+                selectModePanel.SetActive(true);
+                isSlectMode = true;
+            }
+            else
+            {
+                selectModePanel.SetActive(false);
+                isSlectMode = false;
+            }
+        }
+    }
+}
